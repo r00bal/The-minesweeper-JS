4baaf9390d4373a12fb069101d3dca5a98089116
r00bal <frontendpit@gmail.com>
2017-09-22 17:55:47 +0200

    Format the Game Board With printBoard() - continuation.
    Inside the printBoard, log 'Current Board:'.
    Next, call the .join() method on the each row element within your console.log().
    Join using ' | '.

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,5 +1,8 @@
 const printBoard = (board) => {
-
+console.log('Current Board:');
+console.log(board[0].join(' | '));
+console.log(board[1].join(' | '));
+console.log(board[2].join(' | '));
 };
 
 const board = [
@@ -8,4 +11,4 @@ const board = [
   [' ',' ',' ']
 ];
 
-console.log(board);
+printBoard(board);
