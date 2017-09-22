595e17d68218f0db03d53a2c732b447b4ff6d78a
r00bal <frontendpit@gmail.com>
2017-09-22 18:10:19 +0200

    Set Values on the Game Board
    Access the nested array (rows) and set it two random elements.
    One represents a player's guess and the second one is a bomb.
    Log empty board and board in the session.

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,3 +1,9 @@
+const board = [
+  [' ',' ',' '],
+  [' ',' ',' '],
+  [' ',' ',' ']
+];
+
 const printBoard = (board) => {
 console.log('Current Board:');
 console.log(board[0].join(' | '));
@@ -5,10 +11,9 @@ console.log(board[1].join(' | '));
 console.log(board[2].join(' | '));
 };
 
-const board = [
-  [' ',' ',' '],
-  [' ',' ',' '],
-  [' ',' ',' ']
-];
+printBoard(board);
+
+board[0][1] = '1';
+board[2][2] = 'B';
 
 printBoard(board);
