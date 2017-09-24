92c3e80e5585e2455ac31e98e38c8854131eba5d
r00bal <frontendpit@gmail.com>
2017-09-24 18:32:41 +0200

    Rebuild printBoard func., remove commented code

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -4,15 +4,12 @@
 //   [' ',' ',' ']
 // ];
 //
-// const printBoard = (board) => {
-// console.log('Current Board:');
-// console.log(board[0].join(' | '));
-// console.log(board[1].join(' | '));
-// console.log(board[2].join(' | '));
-// };
-//
-// printBoard(board);
-//
+const printBoard = (board) => {
+  console.log(board.map(row => row.join(' | ')).join('\n'));
+};
+
+printBoard(board);
+
 // board[0][1] = '1';
 // board[2][2] = 'B';
 //
