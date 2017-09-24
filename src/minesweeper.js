282618fb71b15f925a2dee3a051ca9a3faadd65b
r00bal <frontendpit@gmail.com>
2017-09-24 16:50:41 +0200

    Add function generateBombBoard
    Function randomly adds bombs to the bomb board.
    It takes 3 arguments: number of board's rows and columns
    and number of bombs.Function generates a random row and column indexes,
    than places a bomb according to those coordinates.
    
    };

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -30,3 +30,28 @@ const generatePlayerBoard = (numberOfRows,numberOfColumns) => {
   return board;
 };
 console.log(generatePlayerBoard(3,3));
+
+const generateBombBoard = (numberOfRows,numberOfColumns, numberOfBombs) => {
+  let board =[];
+  for (let i =0; i < numberOfRows; i++) {
+    let row = [];
+    for (let j =0; j < numberOfColumns; j++) {
+      row.push(null);
+    }
+    board.push(row);
+  }
+
+  let numberOfBombsPlaced = 0;
+  while (numberOfBombsPlaced<numberOfBombs) {
+    let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+    board[randomRowIndex][randomColumnIndex] = 'B';
+    numberOfBombsPlaced ++;
+    // the code in while loop has the potential
+    // to place bombs on top of already existing bombs.
+    // This will be fixed on control flow step of the project.
+  }
+
+  return board;
+};
+console.log(generateBombBoard(6,6, 12));
