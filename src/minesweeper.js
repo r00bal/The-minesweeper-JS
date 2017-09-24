5c381eda109771bf8de148afd92d297666134058
r00bal <frontendpit@gmail.com>
2017-09-24 16:19:26 +0200

    Create a function expression called generatePlayerBoard
    Function takes 2 arguments - number of rows and columns.
    Has two nested for loops that iterates through nested arrays
    to build up the player board.

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -17,3 +17,16 @@
 // board[2][2] = 'B';
 //
 // printBoard(board);
+
+const generatePlayerBoard = (numberOfRows,numberOfColumns) => {
+  let board =[];
+  for (let i =0; i < numberOfRows; i++) {
+    let row = [];
+    for (let j =0; j < numberOfColumns; j++) {
+      row.push(' ')
+    }
+    board.push(row);
+  }
+  return board;
+};
+console.log(generatePlayerBoard(3,3));
