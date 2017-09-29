405748245ac5b8eb2327a75cca30fca36034dff0
r00bal <frontendpit@gmail.com>
2017-09-29 15:27:46 +0200

    Add .generatePlayerBoard() and .generateBombBoard() to the Board class. Make it static.

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -55,47 +55,49 @@ class Board {
 
   print() {
     console.log(this._playerBoard.map(row => row.join(' | ')).join('\n'));
-  };
+  }
 
+  static generatePlayerBoard(numberOfRows,numberOfColumns) {
+    let board =[];
+    for (let i =0; i < numberOfRows; i++) {
+      let row = [];
+      for (let j =0; j < numberOfColumns; j++) {
+        row.push(' ')
+      }
+      board.push(row);
+    }
+    return board;
+  }
+
+  static generateBombBoard(numberOfRows,numberOfColumns, numberOfBombs) {
+    let board =[];
+    for (let i =0; i < numberOfRows; i++) {
+      let row = [];
+      for (let j =0; j < numberOfColumns; j++) {
+        row.push(null);
+      }
+      board.push(row);
+    }
+    let numberOfBombsPlaced = 0;
+    while (numberOfBombsPlaced < numberOfBombs) {
+      let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+      let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+      if (board[randomRowIndex][randomColumnIndex] !== 'B') {
+        board[randomRowIndex][randomColumnIndex] = 'B';
+        numberOfBombsPlaced ++;
+      }
+    }
+    return board;
+  };
 
 }
 
 
 
-const generatePlayerBoard = (numberOfRows,numberOfColumns) => {
-  let board =[];
-  for (let i =0; i < numberOfRows; i++) {
-    let row = [];
-    for (let j =0; j < numberOfColumns; j++) {
-      row.push(' ')
-    }
-    board.push(row);
-  }
-  return board;
-};
 
-const generateBombBoard = (numberOfRows,numberOfColumns, numberOfBombs) => {
-  let board =[];
-  for (let i =0; i < numberOfRows; i++) {
-    let row = [];
-    for (let j =0; j < numberOfColumns; j++) {
-      row.push(null);
-    }
-    board.push(row);
-  }
 
-  let numberOfBombsPlaced = 0;
-  while (numberOfBombsPlaced < numberOfBombs) {
-    let randomRowIndex = Math.floor(Math.random() * numberOfRows);
-    let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
-    if (board[randomRowIndex][randomColumnIndex] !== 'B') {
-      board[randomRowIndex][randomColumnIndex] = 'B';
-      numberOfBombsPlaced ++;
-    }
-  }
 
-  return board;
-};
+
 
 
 
