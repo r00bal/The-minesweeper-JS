6640b292e28f1fb3dc8eb320e535a66024e4cbb9
r00bal <frontendpit@gmail.com>
2017-10-01 12:57:36 +0200

    Add Game class to the game.js

---
+++ src/game.js
@@ -0,0 +1,18 @@
+class Game {
+  constructor(numberOfRows, numberOfColumns, numberOfBombs) {
+    this._board = new Board(numberOfRows, numberOfColumns, numberOfBombs);
+  }
+
+  playMove(rowIndex, columnIndex) {
+    this._board.flipTile(rowIndex, columnIndex);
+    if (this._board.playerBoard[rowIndex][columnIndex] === 'B') {
+    console.log('game over');
+    this._board.print();
+    } else if (this._board.hasSafeTiles()) {
+      console.log('Congratulation, you won!');
+    } else {
+      console.log('Current Board:')
+      this._board.print();
+    }
+  }
+};
